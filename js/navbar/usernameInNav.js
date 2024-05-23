029f9f46041f68814a6cc96dc7e681a128d51f0c
Juan Vinicius <dev.juanvinicius@gmail.com>
2024-05-23 00:52:32 -0300

    improvement: add a user edit info dialog

---
+++ js/navbar/usernameInNav.js
@@ -0,0 +1,20 @@
+import { showProfileInNav } from "./showProfileInNav.js";
+import { showRegisterDialog } from "./userHandler/showRegisterDialog.js";
+import { users } from "../app.js";
+
+export function navbarProfiles() {
+  const selectedProfile = document.getElementById("selected-profile");
+  
+  if (users.length > 0) {
+    selectedProfile.innerText = users[0];
+    selectedProfile.setAttribute("data-user", users[0]);
+  } else {
+    selectedProfile.innerText = "Adicionar novo perfil";
+  }
+
+  const btnAddProfile = document.getElementById("btn-add-profile");
+
+  showProfileInNav();
+
+  btnAddProfile.addEventListener("click", showRegisterDialog);
+}
